91070a2938754b32b7d41e48c76235d9788ed39d
Rayan Subeih <rayanbrogo@gmail.com>
2026-01-29 20:24:32 -0500

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import gsap from 'gsap'
 import {ScrollTrigger, SplitText} from 'gsap/all'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import { Cocktails } from './components/cocktails'
 import About from './components/About'
-
+import Cocktails from './components/Cocktails'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
